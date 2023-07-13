1e927cd628e478bed46a3b75d92e1cb90adf93da
ismailtarihc <0105mert@gmail.com>
2023-07-14 01:11:54 +0300

    initial commit

---
+++ editableShape.js
@@ -0,0 +1,46 @@
+function ShapesTool(){
+	//set an icon and a name for the object
+	this.icon = "assets/shapes.png";
+	this.name = "shapes";
+
+	//to smoothly draw we'll draw a line from the previous mouse location
+	//to the current mouse location. The following values store
+	//the locations from the last frame. They are -1 to start with because
+	//we haven't started drawing yet.
+	var previousMouseX = -1;
+	var previousMouseY = -1;
+
+	this.draw = function(){
+		//if the mouse is pressed
+		if(mouseIsPressed){
+			//check if they previousX and Y are -1. set them to the current
+			//mouse X and Y if they are.
+			if (previousMouseX == -1){
+				previousMouseX = mouseX;
+				previousMouseY = mouseY;
+			}
+			//if we already have values for previousX and Y we can draw a line from 
+			//there to the current mouse location
+			else{
+				line(previousMouseX, previousMouseY, mouseX, mouseY);
+				previousMouseX = mouseX;
+				previousMouseY = mouseY;
+			}
+		}
+		//if the user has released the mouse we want to set the previousMouse values 
+		//back to -1.
+		//try and comment out these lines and see what happens!
+		else{
+			previousMouseX = -1;
+			previousMouseY = -1;
+		}
+	};
+}
+//plot out a shape as a series of vertices
+// add a button for creating new vertices
+//add a button to swithc between new vertices and editing.
+//click the canvas to add a vertex
+// dont draw right away, add vertex to an array, then draw the shape
+//edit the vertices using a mouse drag
+// add a button to edit the vertices
+// give a button to save the shape
