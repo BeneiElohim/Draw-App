625acfd4b46efcfd7890b3a9ce3ab7afe2fa925d
ismailtarihc <0105mert@gmail.com>
2023-07-16 06:03:01 +0300

    Update bucketTool.js

--- bucketTool.js
+++ bucketTool.js
@@ -4,6 +4,23 @@ function BucketTool(){
 	this.name = "bucket";
 
 	this.draw = function(){
+		if(mouseIsPressed){
+			this.floodFill(mouseX, mouseY, get(mouseX, mouseY), selectedColor);
+		}
 	
 	};
+	this.floodFill = function(x, y, targetColor, replacementColor) {
+		if (targetColor === replacementColor) {
+			return;
+		}
+		if (get(x, y) === targetColor) {
+			set(x, y, replacementColor);
+			this.floodFill(x + 1, y, targetColor, replacementColor);
+			this.floodFill(x - 1, y, targetColor, replacementColor);
+			this.floodFill(x, y + 1, targetColor, replacementColor);
+			this.floodFill(x, y - 1, targetColor, replacementColor);
+		}
+	}
+	//TODO Add a tolarence option
+	//TODO Add a fill color option
 }
